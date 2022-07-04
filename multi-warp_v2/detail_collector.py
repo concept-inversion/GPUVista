63700b0e5ae9c59a8c6fcf9867a2dd4d9a5611e1
santosh <santosh.pandey2222@gmail.com>
2022-07-04 14:38:40 -0400

    adds

--- multi-warp_v2/detail_collector.py
+++ multi-warp_v2/detail_collector.py
@@ -18,40 +18,36 @@ from common import *
 
 
 def collector(op_list, op_set):
-    new_list= set(op_list)
-    op_set= op_set.union(new_list)
+	new_list= set(op_list)
+	op_set= op_set.union(new_list)
 
 
 if __name__ == '__main__':
-    #import ipdb; ipdb.set_trace()
-    path= sys.argv[1]
-
-    unique_list= dict.fromkeys(col_name)
-    if(os.path.isdir(path)):
-        directory= os.listdir(path)
-        i=0
-        for f in directory:
-            i=i+1
-            df= pd.read_csv(path+'/'+f, names=col_name,error_bad_lines=False, header=None, )
-            import ipdb; ipdb.set_trace()
-            for col in unique_elements:
-                un=(df[col].unique())
-                if i==1:
-                    unique_list[col]= un
-                else:
-                    #import ipdb; ipdb.set_trace()
-                    temp= set(np.append(unique_list[col],un))
-                    unique_list[col]= np.array(list(temp))
-        import ipdb; ipdb.set_trace()
-    else:
-        df= pd.read_csv(path+'/'+f, names=col_name,error_bad_lines=False, header=None, )
-        for col in unique_elements:
-        	un=(df[col].unique())
-                if i==1:
-                    unique_list[col]= un
-                else:
-                    temp= set(np.append(unique_list[col],un))
-                    unique_list[col]= np.array(list(temp))
-	import ipdb; ipdb.set_trace()
-
-
+	#import ipdb; ipdb.set_trace()
+	path= sys.argv[1]
+	unique_list= dict.fromkeys(col_name)
+	i=0
+	if(os.path.isdir(path)):
+		directory= os.listdir(path)
+		for f in directory:
+			i=i+1
+			df= pd.read_csv(path+'/'+f, names=col_name,error_bad_lines=False, header=None, )
+			import ipdb; ipdb.set_trace()
+			for col in unique_elements:
+				un=(df[col].unique())
+				if i==1:
+					unique_list[col]= un
+				else:
+					#import ipdb; ipdb.set_trace()
+					temp= set(np.append(unique_list[col],un))
+					unique_list[col]= np.array(list(temp))
+		import ipdb; ipdb.set_trace()
+	else:
+		df= pd.read_csv(path, names=col_name,error_bad_lines=False, header=None, )
+		for col in unique_elements:
+			un=(df[col].unique())
+			if i==1:
+				unique_list[col]= un
+			else:
+				temp= set(np.append(unique_list[col],un))
+				unique_list[col]= np.array(list(temp))
